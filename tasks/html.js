2ad92e793e7af9135901424eec298319cc2b30a1
Rustam Holiev <golevv.rustam@gmail.com>
2024-04-27 07:38:36 +0300

    refactoring

--- tasks/html.js
+++ tasks/html.js
@@ -1,18 +1,22 @@
-const {src,dest} = require('gulp');
+const {src, dest} = require('gulp');
 const include = require('gulp-file-include');
 const bs = require('browser-sync');
 const prettify = require('gulp-html-prettify');
 
 module.exports = function html() {
-	return src(['src/**/*.html', '!src/components/**/*.html'])
-		.pipe(include({
-			prefix: '@',
-			basepath: '@file',
-		}))
-		.pipe(prettify({
-			indent_char: '	',
-			indent_size: 1
-		}))
-		.pipe(dest('build'))
-		.pipe(bs.stream())
+    return src([
+        'src/**/*.html',
+        '!src/components/**/*.html',
+        '!src/blocks/**/*.html'
+    ])
+        .pipe(include({
+            prefix: '@',
+            basepath: '@file',
+        }))
+        .pipe(prettify({
+            indent_char: '	',
+            indent_size: 1
+        }))
+        .pipe(dest('build'))
+        .pipe(bs.stream())
 }
